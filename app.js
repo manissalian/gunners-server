1863071bcaa24f28cecf9a0d509a27f02798dc41
Raffi <rmanisalian@cinemoz.com>
2019-09-15 14:00:10 +0300

    app init, match endpoints: create and get all

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const app = express()
+
+const match = require('./routes/match.js')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/match/all', match.getAll)
+app.post('/match/create', match.create)
+
+module.exports = app
---
+++ routes/match.js
@@ -0,0 +1,58 @@
+const nano = require('nano')('http://localhost:5984')
+const gunnersDb = nano.use('gunners')
+
+module.exports = {
+  create: (req, res, next) => {
+    const {
+      players,
+      subs,
+      teamGoals,
+      opponent,
+      result,
+      ground,
+      tournament,
+      date
+    } = req.body
+
+    if (!opponent || !tournament) {
+      res.status(400).send('Make sure to pass opponent and tournament')
+      return
+    }
+
+    if (!opponent.name || !opponent.asset) {
+      res.status(400).send('opponent must be of format: { name: string, asset: string }')
+      return 
+    }
+
+    const params = {
+      type: 'match',
+      players: players || [],
+      subs: subs || [],
+      teamGoals: teamGoals || [],
+      opponent: {
+        ...opponent,
+        goals: 0
+      },
+      result: result || null,
+      ground: {
+        type: ground && ground.type || null,
+        name: ground && ground.name || null
+      },
+      tournament,
+      date: date || null
+    }
+
+    gunnersDb.insert(params)
+    .then(body => {
+      res.send(body)
+    })
+  },
+
+  getAll: (req, res, next) => {
+    gunnersDb.view('match', 'byOpponent', {
+      include_docs: true
+    }).then(body => {
+      res.send(body)
+    })
+  }
+}
